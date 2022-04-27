c771b9cd71624b96e95affa6f1632e8bd6d820dd
cdgeass <cdgeass97@gmail.com>
2022-04-27 18:15:46 +0800

    Fix avatar throw error when user with noavatar

--- lib/components/avatar.dart
+++ lib/components/avatar.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:keylol_flutter/common/constants.dart';
 
 enum AvatarSize {
@@ -39,27 +41,61 @@ class Avatar extends StatelessWidget {
   Widget build(BuildContext context) {
     final avatarUrl = size.avatarUrl + uid;
 
-    final avatar = CachedNetworkImage(
-      width: width,
-      height: width,
-      imageUrl: avatarUrl,
-      placeholder: (context, url) => Image.asset('images/unknown_avatar.jpg'),
-      errorWidget: (context, url, error) =>
-          Image.asset('images/unknown_avatar.jpg'),
-    );
+    return FutureBuilder(
+        future: _isSvg(avatarUrl),
+        builder: (context, AsyncSnapshot<bool> snapshot) {
+          late Widget avatar;
+          if (snapshot.hasData) {
+            if (!snapshot.data!) {
+              avatar = CachedNetworkImage(
+                width: width,
+                height: width,
+                imageUrl: avatarUrl,
+                placeholder: (context, url) =>
+                    Image.asset('images/unknown_avatar.jpg'),
+                errorWidget: (context, url, error) =>
+                    Image.asset('images/unknown_avatar.jpg'),
+              );
+            } else {
+              avatar = Image.asset(
+                'images/unknown_avatar.jpg',
+                width: width,
+                height: width,
+              );
+            }
+          } else {
+            avatar = Image.asset(
+              'images/unknown_avatar.jpg',
+              width: width,
+              height: width,
+            );
+          }
 
-    late Widget child;
-    if (clip) {
-      child = ClipOval(
-        child: avatar,
-      );
-    } else {
-      child = avatar;
+          late Widget child;
+          if (clip) {
+            child = ClipOval(
+              child: avatar,
+            );
+          } else {
+            child = avatar;
+          }
+          return InkWell(
+              onTap: () {
+                Navigator.of(context).pushNamed('/space', arguments: uid);
+              },
+              child: child);
+        });
+  }
+
+  Future<bool> _isSvg(avatarUrl) async {
+    final file = await DefaultCacheManager().getFileFromCache(avatarUrl);
+    if (file != null) {
+      return false;
     }
-    return InkWell(
-        onTap: () {
-          Navigator.of(context).pushNamed('/space', arguments: uid);
-        },
-        child: child);
+
+    final response = await Dio().head(avatarUrl);
+    return response.redirects.any((redirect) {
+      return redirect.location.path.contains('noavatar.svg');
+    });
   }
 }
